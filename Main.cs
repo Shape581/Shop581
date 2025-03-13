32cfddb5e8ce2581467285bb68f3f8a9dc3e4eb7
Shape581 <severusrogue5008@gmail.com>
2025-03-13 11:02:15 +0100

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,35 @@
+﻿using Life;
+using ModKit.Interfaces;
+using ModKit.Internal;
+using RTG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop581
+{
+    public class Main : ModKit.ModKit
+    {
+        public static PluginInformations Informations { get; private set; } = new PluginInformations("Shop581", "1.0.0", "Shape581");
+
+        ///////////////////////////////////////////////
+        //Ce Code n'est pas le code complet du Plugin//
+        ///////////////////////////////////////////////
+
+        public Main(IGameAPI api) : base(api) 
+        {
+            PluginInformations = new ModKit.Interfaces.PluginInformations("Shop581", "1.0.0", "Shop581");
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            Orm.RegisterTable<Shop581.ShopPoints>();
+            Orm.RegisterTable<Shop581.ShopItems>();
+            Orm.RegisterTable<Shop581.ShopPaterns>();
+            Logger.LogSuccess(PluginInformations.SourceName, "Initialise !");
+        }
+    }
+}
